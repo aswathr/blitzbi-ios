620b29c9ddc90eefaf60c5ab9755489f27d2c94e
Akash Chetty <achetty@kiwiup.com>
2020-09-30 11:09:34 +0530

    Initial Commit

---
+++ BI/BI.h
@@ -0,0 +1,18 @@
+//
+//  BI.h
+//  BI
+//
+//  Created by Admin on 30/09/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BI.
+FOUNDATION_EXPORT double BIVersionNumber;
+
+//! Project version string for BI.
+FOUNDATION_EXPORT const unsigned char BIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BI/PublicHeader.h>
+
+
