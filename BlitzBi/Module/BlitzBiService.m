94ff9af16512d167e85ea8c6dfdca082d8ea0539
Akash Chetty <achetty@kiwiup.com>
2020-10-14 17:00:48 +0530

    updated the base url

--- BlitzBi/Module/BlitzBiService.m
+++ BlitzBi/Module/BlitzBiService.m
@@ -23,7 +23,7 @@
 
 - (void)setUp:(NSString*)appId
              :(NSString*)appToken {
-    baseUrl = @"https://blitzbi-dev.useblitz.com/";
+    baseUrl = @"https://bprod-blitzbi.useblitz.com/";
     baseUrls = [[BaseUrls alloc] init:baseUrl];
     
     BlitzNetworkModuleBuilder *networkBuilder = [[BlitzNetworkModuleBuilder alloc] init];
