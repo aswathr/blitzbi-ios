80c98878a4bab2ffdea98308aad42e93a5459146
Aswath Narayanan <aswathrce@gmail.com>
2022-03-24 16:16:44 +0530

    Renaming library package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BlitzBi",
-            targets: ["blitzbi-ios"]),
+            targets: ["BlitzBi"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "blitzbi-ios",
+            name: "BlitzBi",
             dependencies: [],
             path: "Sources/",
             publicHeadersPath: "Public/",
